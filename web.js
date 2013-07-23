16c0f3e49b38d77e88bf4cdc4b85f59a91c9e7de
Esko Laaksonen <esko.laaksonen@gmail.com>
2013-07-23 05:02:46 +0000

    Added marketing material and images

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html").toString());
 });
